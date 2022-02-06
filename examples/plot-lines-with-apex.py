bd6e7bd2f4d735a4618d9c727f41c900e6a95672
Lukasz Migas <lukas.migas@yahoo.com>
2022-02-06 16:27:20 +0100

    Updated examples

--- examples/plot-lines-with-apex.py
+++ examples/plot-lines-with-apex.py
@@ -5,9 +5,11 @@ import numpy as np
 viewer1d = napari_plot.Viewer()
 x = np.arange(0.0, 10.0, 0.01)
 y = np.sin(2 * np.pi * x)
-peaks_x = x[np.where(y > 0.95)]
 viewer1d.add_line(np.c_[x, y], name="Sin", color="#FF0000")
-viewer1d.add_inf_line(peaks_x, orientation="vertical", color="#00FFFF")
+peaks_x = x[np.where(y == 1.0)]
+viewer1d.add_inf_line(peaks_x, orientation="vertical", color="#00FFFF", opacity=0.5, name="Max")
+peaks_x = x[np.where(y == -1.0)]
+viewer1d.add_inf_line(peaks_x, orientation="vertical", color="#FFFF00", opacity=0.5, name="Min")
 viewer1d.axis.x_label = "time (s)"
 viewer1d.axis.y_label = "voltage (mV)"
 napari_plot.run()
--- examples/plot-lines-with-span.py
+++ examples/plot-lines-with-span.py
@@ -8,7 +8,13 @@ y = np.sin(2 * np.pi * x)
 pos_x = x[np.where(y == 1.0)]
 window = 0.2
 viewer1d.add_line(np.c_[x, y], name="Sin", color="#FF0000")
-viewer1d.add_region([(p - window, p + window) for p in pos_x], orientation="vertical", face_color="#00FFFF")
+viewer1d.add_region(
+    [(p - window, p + window) for p in pos_x],
+    orientation="vertical",
+    face_color="#00FFFF",
+    opacity=0.2,
+    name="Apex windows",
+)
 viewer1d.axis.x_label = "time (s)"
 viewer1d.axis.y_label = "voltage (mV)"
 napari_plot.run()
